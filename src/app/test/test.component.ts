4d505fb48dc0e0ccaf21c54aa83208fd07c2aa78
iamafsana <afsanaabida@yahoo.com>
2019-01-16 11:58:07 +0600

    class binding handled

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,16 +6,36 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       Welcome {{name}}
     </h2>
-    <input [id]="myId" type="text" value="Khushbu">
-    <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Khushbu">
+    <h2 class="text-special">Afsana</h2>
+    <h2 [class]="successClass">Afsana</h2>
+    <h2 class="text-special" [class]="successClass">Afsana</h2>
+    <h2 [class.text-danger]="hasError">Afsana</h2>
+    <h2 [ngClass]="messageClasses">Afsana</h2>
   `,
-  styles: []
+  styles: [`
+  .text-success {
+    color: green;
+  }
+
+  .text-danger {
+    color:red;
+  }
+  .text-special {
+    font-style: italic;
+  }
+  
+`]
 })
 export class TestComponent implements OnInit {
   public name = "Afsana";
-  public myId = "testId";
-  public isDisabled = false;
- 
+  public successClass= "text-success";
+  public hasError = true;
+  public isSpecial=true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger":this.hasError,
+    "text-special": this.isSpecial
+  }
   constructor() { }
 
   ngOnInit() {
