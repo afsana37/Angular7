0ef87f221a3eb40c0fec6dc3ca74d3de31f0f8ea
iamafsana <afsanaabida@yahoo.com>
2019-01-16 13:32:16 +0600

    style binding done

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,36 +6,23 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       Welcome {{name}}
     </h2>
-    <h2 class="text-special">Afsana</h2>
-    <h2 [class]="successClass">Afsana</h2>
-    <h2 class="text-special" [class]="successClass">Afsana</h2>
-    <h2 [class.text-danger]="hasError">Afsana</h2>
-    <h2 [ngClass]="messageClasses">Afsana</h2>
+    <h2 [style.color]="'orange'">Style Binding</h2>
+    <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding 1</h2>
+    <h2 [style.color]="highlightColor">Style Binding 2</h2>
+    <h2 [ngStyle]="titleStyles">Style Binding 3</h2>
   `,
-  styles: [`
-  .text-success {
-    color: green;
-  }
-
-  .text-danger {
-    color:red;
-  }
-  .text-special {
-    font-style: italic;
-  }
-  
-`]
+  styles: []
 })
 export class TestComponent implements OnInit {
   public name = "Afsana";
-  public successClass= "text-success";
   public hasError = true;
-  public isSpecial=true;
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger":this.hasError,
-    "text-special": this.isSpecial
+  public isSpecial = true;
+  public highlightColor = "orange";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
   }
+ 
   constructor() { }
 
   ngOnInit() {
