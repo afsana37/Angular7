a73e337e36c62e0c1a6b6c27b0d6a3a42d2b2f0c
iamafsana <afsanaabida@yahoo.com>
2019-01-16 13:44:15 +0600

    event binding done

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,26 +6,24 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       Welcome {{name}}
     </h2>
-    <h2 [style.color]="'orange'">Style Binding</h2>
-    <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding 1</h2>
-    <h2 [style.color]="highlightColor">Style Binding 2</h2>
-    <h2 [ngStyle]="titleStyles">Style Binding 3</h2>
+    <button (click)="onClick($event)">Greet</button>
+    <button (click)="greeting='Welcome Afsana'">Greet</button>
+    {{greeting}}
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
   public name = "Afsana";
-  public hasError = true;
-  public isSpecial = true;
-  public highlightColor = "orange";
-  public titleStyles = {
-    color: "blue",
-    fontStyle: "italic"
-  }
+  public greeting="";
  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(event){
+    console.log(event);
+    this.greeting = 'Welcome to Kbookwiz';
   }
 
  
