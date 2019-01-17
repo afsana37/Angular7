071d0631061f6a1edf19b0f04688d5bfcccee7c9
iamafsana <afsanaabida@yahoo.com>
2019-01-17 14:57:47 +0600

    pipe

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,18 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: "app-test",
+  selector: 'app-test',
   template: `
-    <div *ngFor="let color of colors; even as e">
-      <h2>{{e}} {{color}}</h2>
-    </div>
+    <h2>
+      {{"Hello " + name}}
+    </h2>
+    <button (click)=fireEvent()>Send Event</button>
+    <h2>{{"Codevolution" | lowercase}}</h2>
+    <h2>{{"Codevolution" | uppercase}}</h2>
+    <h2>{{"Welcome to codevolution" | titlecase}}</h2>
+    <h2>{{"Codevolution" | slice:3:6}}</h2>
+    <h2>{{person | json}}</h2>
+    <h2>{{5.678 | number:'1.2-3'}}</h2>
+    <h2>{{5.678 | number:'3.4-5'}}</h2>
+    <h2>{{5.678 | number:'3.1-2'}}</h2>
+    
+    
+    <h2>{{0.25 | percent}}</h2>
+    <h2>{{0.25 | currency}}</h2>
+    <h2>{{0.25 | currency:'GBP'}}</h2>
+    <h2>{{0.25 | currency:'GBP':'code'}}</h2>
+    
+    <h2>{{date}}</h2>
+    <h2>{{date | date:'short'}}</h2>
+    <h2>{{date | date:'shortDate'}}</h2>
+    <h2>{{date | date:'shortTime'}}</h2>
+    
+    
+    
+    
+    
+    
+    
+    
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  public colors =  ["red", "blue", "green", "yellow"];
 
-  constructor() {}
+  @Input('parentData') public name;
+  @Output() public childEvent = new EventEmitter<string>();
 
-  ngOnInit() {}
+  public person = {
+    "firstName": "John",
+    "lastName": "Doe"
+  }
+
+  public date = new Date();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  fireEvent(){
+    this.childEvent.emit('Hey Codevolution');
+  }
+
 }
